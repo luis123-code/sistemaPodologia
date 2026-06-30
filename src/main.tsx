c0166e111d5f3123846cb62561238192a9509180
Luis Ramirez Zevallos <albetamirez@gmail.com>
2026-06-30 08:35:09 -0500

    se corrigio validaciones en producciones

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,21 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// import { isValidJWT } from "./lib/auth";
+import { isValidJWT } from "./lib/auth";
 
-// const urlParams = new URLSearchParams(window.location.search);
-// const urlToken = urlParams.get("token");
-// const storedToken = localStorage.getItem("token");
-// const token = urlToken || storedToken;
+const urlParams = new URLSearchParams(window.location.search);
+const urlToken = urlParams.get("token");
+const storedToken = localStorage.getItem("token");
+const token = urlToken || storedToken;
 
-// if (!isValidJWT(token)) {
-//   localStorage.removeItem("token");
-//   window.location.href = "https://login-podologiazevallos.vercel.app/?token=true";
-// } else {
-//   if (urlToken) {
-//     localStorage.setItem("token", urlToken);
-//     window.history.replaceState({}, document.title, window.location.pathname);
-//   }
-//   createRoot(document.getElementById("root")!).render(<App />);
-// }
+console.log("[Auth] urlToken:", urlToken);
+console.log("[Auth] storedToken:", storedToken);
+console.log("[Auth] token usado:", token);
+console.log("[Auth] isValidJWT:", isValidJWT(token));
+
+if (urlToken) {
+  localStorage.setItem("token", urlToken);
+  window.history.replaceState({}, document.title, window.location.pathname);
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
